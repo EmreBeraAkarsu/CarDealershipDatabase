a6381b01f06b6d81e66ada636420323a97c580e1
Emre Akarsu <akarsutech@gmail.com>
2024-11-22 14:16:17 -0500

    Initial commit

---
+++ CarDealershipDatabase.sql
@@ -0,0 +1,33 @@
+DROP DATABASE if exists CarDealershipDatabase; 
+
+create database CarDealershipDatabase;
+
+use CarDealershipDatabase;
+
+drop table if exists ;
+
+
+CREATE TABLE dealerships(
+dealership_id datatype,
+`name` datatype,
+address datatype,
+phone varchar(12)
+);
+
+
+
+
+CREATE TABLE vehicles
+
+
+
+CREATE TABLE inventory
+
+
+
+CREATE TABLE sales_contracts
+
+
+
+CREATE TABLE lease_contracts
+
